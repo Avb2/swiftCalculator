51241d75ec99f08938da52ffd5deb3debc5a8bba
Avb2 <bringuel.alexander@gmail.com>
2025-03-12 00:00:36 -0400

    EZ

--- swiftCalculator/ViewController.swift
+++ swiftCalculator/ViewController.swift
@@ -8,11 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var number1Input: UITextField!
+    @IBOutlet weak var number2Input: UITextField!
+    @IBOutlet weak var answerOutput: UILabel!
+    
+    
+    @IBAction func addBtn(_ sender: UIButton) {
+        let num1 = Double(number1Input.text ?? "") ?? 0
+        let num2 = Double(number2Input.text ?? "") ?? 0
+        answerOutput.text = "\(num1 + num2)"
+    }
 
+    @IBAction func minusBtn(_ sender: UIButton) {
+        let num1 = Double(number1Input.text ?? "") ?? 0
+        let num2 = Double(number2Input.text ?? "") ?? 0
+        answerOutput.text = "\(num1 - num2)"
+    }
+
+    @IBAction func divideBtn(_ sender: UIButton) {
+        let num1 = Double(number1Input.text ?? "") ?? 0
+        let num2 = Double(number2Input.text ?? "") ?? 1
+        answerOutput.text = "\(num1 / num2)"
+    }
+
+    @IBAction func multiplyBtn(_ sender: UIButton) {
+        let num1 = Double(number1Input.text ?? "") ?? 0
+        let num2 = Double(number2Input.text ?? "") ?? 0
+        answerOutput.text = "\(num1 * num2)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+        number1Input.clearsOnBeginEditing = true
+        number2Input.clearsOnBeginEditing = true
+       
     }
+    
+    
+    @objc func dismissKeyboard() {
+           view.endEditing(true)
+       }
 
 
 }
